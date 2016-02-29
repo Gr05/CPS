14d38d3bb24e834f3352f732a52638c668e4e9d7
s6mon <sim.chambonnet@gmail.com>
2016-02-29 11:11:36 +0100

    init tp4

---
+++ TP4.c
@@ -0,0 +1,18 @@
+/**********TP4**********/
+/***Simon CHAMBONNET****/
+/*****Lucas GUERRY******/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*bibliothèques nécessaire pour exploiter les propriétées d'un fichier*/
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ #include <time.h>
+
+
+/*--------------------- Exercice 1 ----------------------*/
+
+//Question 1
