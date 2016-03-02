6bb129037bda40a8dbc5553e5881c412570b4d0f
Gr05 <lucas.guerry@gmail.com>
2016-03-02 16:04:44 +0100

    début TP5

--- TP5.c
+++ TP5.c
@@ -1,27 +1,10 @@
-/**********TP4**********/
+/**********TP5**********/
 /***Simon CHAMBONNET****/
 /*****Lucas GUERRY******/
 
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-
-/*bibliothèques nécessaire pour exploiter les propriétées d'un fichier*/
- #include <sys/types.h>
- #include <sys/stat.h>
- #include <time.h>
 
 
 /*--------------------- Exercice 1 ----------------------*/
-
-/*Fonction my_strlen*/
-int my_strlen (char * chaine)
-{
-	int i = 0;
-	while (*chaine++)
-	{
-		i++;
-	}
-	return i;
-}
