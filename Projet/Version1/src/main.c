cdb2f97098c31107ed1c2d4b22a902b1ec4ae5a8
Gr05 <lucas.guerry@gmail.com>
2016-03-29 14:38:13 +0200

    bon main

--- Projet/Version1/src/main.c
+++ Projet/Version1/src/main.c
@@ -5,39 +5,6 @@
 
 
 int main (int argc, char * argv[]){
-<<<<<<< HEAD
-	int i,j,a;
-	grille g;
-	int taille = 10;
-	FILE * fichier;
-	carac_navires tab_def_jeu[4]; //tableau ou sont associé les types au nombre
-
-	if (argc != 2)
-	{
-		printf("Il n'y a pas le bon nombre d'arguments\n");
-		exit(1);
-	}
-	
-	g = creer_grille(taille);
-	fichier = open_file(argv[1]);
-
-	init_grille (g, taille);
-	init_carac_navires (tab_def_jeu);
-
-	remplit_grille (g, taille, fichier, tab_def_jeu);
-
-	liste_navires * l = creer_liste_navires(g, taille);
-
-	afficher_liste(l);
-
-
-	//mauvaise conv !!
-	g[0][9] = 'T';
-	g[1][9] = 'T';
-	
-	a = un_navire_coule(l, 1, 9, g);
-	printf("%d\n", a);
-=======
 	int i,j, taille, nb_coup;
 	grille g, gc;
 	liste_navires * l;
@@ -78,7 +45,6 @@ int main (int argc, char * argv[]){
 		joue(g, gc, taille, l, i, j);
 		nb_coup++;
 		//afficher gc
->>>>>>> e4472b2a1a760f9b826de4eb04b66b1192c5334f
 
 	}
 	printf("Félicitation le jeu est terminé !!\nIl vous a fallut %d coup pour coulé tout les bateaux\n", nb_coup);
